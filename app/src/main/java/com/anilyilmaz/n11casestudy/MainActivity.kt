4a136bb8551f921a56fe5d7db73c296cd35a691a
anilyilmaz <anil.yilmaz@venhancer.com>
2024-03-14 00:31:36 +0300

    Add theme

--- app/src/main/java/com/anilyilmaz/n11casestudy/MainActivity.kt
+++ app/src/main/java/com/anilyilmaz/n11casestudy/MainActivity.kt
@@ -10,7 +10,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.anilyilmaz.n11casestudy.ui.theme.N11CaseStudyTheme
+import com.anilyilmaz.n11casestudy.core.designsystem.theme.N11CaseStudyTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
