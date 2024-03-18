75f98b7156e945e94b009e04225bf89e82af7ebd
anilyilmaz <anil.yilmaz@venhancer.com>
2024-03-18 07:35:21 +0300

    Add README

---
+++ README.md
@@ -0,0 +1,14 @@
+Case study for N11. It follows Android's architecture guideline and entirely written with Kotlin.
+
+## Screenshots
+<img src="/docs/images/list.png" width="270"> <img src="/docs/images/detail.png" width="270">
+
+## Tech Stack
+* Kotlin
+* Coroutines & Flow
+* Android Architecture Components
+* Dagger Hilt
+* Coil
+* Retrofit
+* OkHttp
+* Paging
---
+++ app/src/main/java/com/anilyilmaz/n11casestudy/core/testing/testDoubles/modelfactory/NetworkUserDetailTestData.kt
@@ -0,0 +1,38 @@
+package com.anilyilmaz.n11casestudy.core.testing.testDoubles.modelfactory
+
+import com.anilyilmaz.n11casestudy.core.network.model.NetworkUserDetail
+
+fun networkUserDetailTestData() = NetworkUserDetail (
+    id = 100,
+    login = "uyelikanil",
+    nodeId = "",
+    avatarUrl = "https://avatars.githubusercontent.com/u/56436145?v=4",
+    gravatarId = "",
+    url = "",
+    htmlUrl = "",
+    followersUrl = "",
+    followingUrl = "",
+    gistsUrl = "",
+    starredUrl = "",
+    subscriptionsUrl = "",
+    organizationsUrl = "",
+    reposUrl = "",
+    eventsUrl = "",
+    receivedEventsUrl = "",
+    type = "",
+    siteAdmin = false,
+    name = "",
+    company = "",
+    blog = "",
+    location = "",
+    email = "",
+    hireable = false,
+    bio = "",
+    twitterUserName = "",
+    publicRepos = 0,
+    publicGists = 0,
+    followers = 0,
+    following = 0,
+    createdAt = "",
+    updatedAt = ""
+)
